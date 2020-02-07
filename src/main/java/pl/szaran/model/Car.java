2c89d9e9c9dcdc94a95970a8a57c9d8d08726b17
Peter <piotr.szaran@gmail.com>
2020-02-07 17:09:01 +0100

    v6 - Model - Car updated. added constructor and @Override toString() method.

--- src/main/java/pl/szaran/model/Car.java
+++ src/main/java/pl/szaran/model/Car.java
@@ -1,8 +1,12 @@
 package pl.szaran.model;
 
 import lombok.Getter;
+import pl.szaran.converters.CarJsonConverter;
+import pl.szaran.exceptions.MyException;
+import pl.szaran.validators.CarValidator;
 
 import java.math.BigDecimal;
+import java.util.Map;
 
 /**
  * Klasa Car posiada pola model (napis składający się tylko i
@@ -23,4 +27,36 @@ public class Car {
     private Wheel wheel;
     private CarBody carBody;
 
+    public Car(String jsonFilename) {
+        CarJsonConverter carJsonConverter = new CarJsonConverter(jsonFilename);
+
+        CarValidator carValidator = new CarValidator();
+
+        var car = carJsonConverter.fromJson().orElseThrow(() -> new MyException("JSON PARSE EXCEPTION IN CAR CONSTRUCTOR"));
+
+        Map<String, String> errors = carValidator.validate(car);
+
+        if (carValidator.hasErrors()) {
+            errors.forEach((k, v) -> System.out.println(k + " " + v));
+            throw new MyException("ERRORS IN JSON FILE: " + jsonFilename);
+        }
+
+        this.model = car.model;
+        this.price = car.price;
+        this.mileage = car.mileage;
+        this.engine = car.engine;
+        this.wheel = car.wheel;
+        this.carBody = car.carBody;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "\nmodel:'" + model + '\'' +
+                        ", \n  price:" + price +
+                        ", \n   mileage:" + mileage +
+                        ", \n   engine:" + engine +
+                        ", \n    wheel:" + wheel +
+                        ", \n    carBody:" + carBody;
+    }
 }
